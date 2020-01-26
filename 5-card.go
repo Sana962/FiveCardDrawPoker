a23c677e5067cc1b6fc137ebf74a00feb653fd39
Sana962 <a962471@outlook.com>
2020-01-26 12:16:45 +0500

    Declration and initialization of an array of hand values

--- 5-card.go
+++ 5-card.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 
-/* Group all hand value  like enum in c++*/
+/* Group all (related values )hand value like enum in c++*/
 const 
 (
     HIGHEST_CARD = 0
@@ -17,17 +17,18 @@ const
 	STRAIGHT_FLUSH
 )
 
+/* Declration and initialization of a pointer array */
+var *hands[] char
 
-
-
-
-
-
-
-
-
-
-
+hands[0] = "highest-card"
+hands[1] = "one-pair"
+hands[2] = "two-pairs"
+hands[3] = "three-of-a-kind"
+hands[4] = "straight"
+hands[5] = "flush"
+hands[6] = "full-house"
+hands[7] = "four-of-a-kind"
+hands[8] = "straight-flush"
 
 
 func main() {
