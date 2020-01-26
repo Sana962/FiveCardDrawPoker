12a80ebe5c277eb7049e3af1c823c38731f1ce72
Sana962 <a962471@outlook.com>
2020-01-26 12:38:48 +0500

    Function implementation for hand value (Flush)

--- 5-card.go
+++ 5-card.go
@@ -30,9 +30,30 @@ hands[6] = "full-house"
 hands[7] = "four-of-a-kind"
 hands[8] = "straight-flush"
 
+/* declaration of arrays of hand card , deck and card which is currently in play */
 var hand[5][3]char
 var hand[5][3]char
 var hand[5][3]char
+
+/* Function for hand value Flush */
+func Flush() int 
+{
+   var i int;
+   var c char;
+   c = play[0][1];                  /* c stores suit to which card belongs */
+
+   for i:= 1 ; i<5 ; i++
+   {
+	   if c!=play[i][1]              /* compares the next card's suit */
+	   {
+		   return 0;
+	   }
+   }
+   return 1;                        /* return 1 if all card belong to same suit
+}
+
+
+
 
 
 func main() {
