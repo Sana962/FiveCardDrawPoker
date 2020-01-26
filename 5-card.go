a7a9d3c0dd2290051ebbd0ba7cf809eb2aa61463
Sana962 <a962471@outlook.com>
2020-01-26 11:22:33 +0500

    Add Group of hand values

---
+++ 5-card.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+
+/* Group all hand value  like enum in c++*/
+const 
+(
+    HIGHEST_CARD = 0
+	ONE_PAIR
+	TWO_PAIRS
+	THREE_OF_A_KIND
+	STRAIGHT
+	FLUSH
+	FULL_HOUSE
+	FOUR_OF_A_KIND
+	STRAIGHT_FLUSH
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func main() {
+
+}
